d9a059f9fde472e62230fee1022538d7af9e4e10
python_pyb <dbqls3698@naver.com>
2025-07-12 21:21:25 +0900

    미니룸 가구 배치

--- Frontend/src/pages/MiniRoom.jsx
+++ Frontend/src/pages/MiniRoom.jsx
@@ -1,19 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './MiniRoom.css';
 
+const imageList = [
+  'bed.png',
+  'table_01.png',
+  'chair.png',
+  'bookcase.png',
+  'clock.png',
+  'picture.png',
+  'wall_01.png',
+  'closet.png',
+  'side_table.png',
+  'sitting_mat.png',
+  // 필요시 추가
+];
+
 const MiniRoom = () => {
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setCurrentTime(new Date());
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, []);
-
-
   return (
     <div className="miniroom">
+      {imageList.map((img, idx) => (
+        <img
+          key={img}
+          src={`/img/${img}`}
+          alt={img.replace('.png', '')}
+          className={`miniroom-img miniroom-img-${idx}`}
+        />
+      ))}
     </div>
   );
 };
