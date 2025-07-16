a59619b2f6f5405a54f491855a33c43db0dc321d
BSEom <ebs970725@gmail.com>
2025-07-16 12:09:25 +0900

    backend 갱신

---
+++ Backend/miniworld/src/main/java/com/pknu/miniworld/MiniworldApplication.java
@@ -0,0 +1,13 @@
+package com.pknu.miniworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniworldApplication.class, args);
+	}
+
+}
