db04cb1aca587ec3dfbfc8df0895add0ac92e39f
python <113078344+ManiacMemories@users.noreply.github.com>
2025-07-23 11:30:13 +0900

    registertest
    회원가입 수정

--- Backend/miniworld/src/main/java/com/pknu/miniworld/Miniroom/Controller/MiniroomController.java
+++ Backend/miniworld/src/main/java/com/pknu/miniworld/Miniroom/Controller/MiniroomController.java
@@ -1,5 +1,25 @@
 package com.pknu.miniworld.Miniroom.Controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pknu.miniworld.Miniroom.Entity.MiniroomEntity;
+import com.pknu.miniworld.Miniroom.Service.MiniroomService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/minirooms")
+@RequiredArgsConstructor
 public class MiniroomController {
-    
+    private final MiniroomService miniroomService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<MiniroomEntity> getMiniroomByUserId(@PathVariable Long userId) {
+        MiniroomEntity miniroom = miniroomService.getMiniroomByUserId(userId);
+        return ResponseEntity.ok(miniroom);
+    }
 }
--- Backend/miniworld/src/main/java/com/pknu/miniworld/User/DTO/UserDTO.java
+++ Backend/miniworld/src/main/java/com/pknu/miniworld/User/DTO/UserDTO.java
@@ -4,21 +4,33 @@ import java.time.LocalDate;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UserDTO {
-    @NotBlank
+    @NotBlank(message = "아이디는 필수 항목입니다.")
+    @Size(min = 4, max = 12, message = "아이디는 4 ~ 12자여야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "아이디는 영문 또는 숫자만 입력 가능합니다.")
     private String username;
 
-    @NotBlank
+    @NotBlank(message = "비밀번호는 필수 항목입니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8~20자여야 합니다.")
+    @Pattern(
+        regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+=-])[A-Za-z\\d!@#$%^&*()_+=-]{8,20}$",
+        message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다."
+    )
+
     private String password;
 
-    @NotBlank
+    @NotBlank(message = "닉네임은 필수 항목입니다.")
+    @Size(min = 2, max = 10, message = "닉네임은 2~10자여야 합니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9가-힣]*$", message = "닉네임은 한글, 영문, 숫자만 사용할 수 있습니다.")
     private String nickname;
 
-    @Email
-    @NotBlank
+    @NotBlank(message = "이메일은 필수 항목입니다.")
+    @Email(message = "올바른 이메일 형식이어야 합니다.")
     private String email;
 
     private LocalDate birthDate;
